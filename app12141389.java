9d648a4bb13603924b2f994309bd0ed1e9aa159b
antokgnd <pemudat45@gmail.com>
2016-11-22 13:10:29 +0700

    12141389 - sebuah pengeluaran nilai yg dimana akan bernilai 1 jika game over

--- app12141389.java
+++ app12141389.java
@@ -7,5 +7,18 @@ import java.util.Scanner;
  * @author Antok
  */
 public class app12141389 {
-    
-}
+ public static void main(String[] args) {
+        ArrayList <String> namaPemain = new ArrayList<>(); 
+        ArrayList <Integer> skorPermainan = new ArrayList<>(); 
+        Scanner input = new Scanner(System.in);
+        Scanner input2= new Scanner(System.in);
+        Random angkaRandom = new Random();
+        String cari Y;
+        
+        do{
+            System.out.println("Masukkan 'Nama' anda atau tekan 'y' untuk keluar, kemudian tekan enter = ");
+            cariY=input.nextLine();
+            namaPemain.add(cariY);
+            int NilaiJalan = 0; 
+            int CekGameOver=0;
+
